566d433f0b99d4696a2574e8de895dc2ada266c1
CHAIKARAN <112747168+CHAIKARAN@users.noreply.github.com>
2023-11-03 13:23:07 +0530

    Update auth.js

--- models/auth.js
+++ models/auth.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
     email: {type:String , require: true},
     name: {type:String},
-    desc: {type;String},
+    desc: {type:String},
     joinedOn: {type:Date,default:Date.now}
 })
 
